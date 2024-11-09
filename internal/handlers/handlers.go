8ad72537ac2eadf9c271844af3bd17b53c9c66a9
mhson281 <mhson281@gmail.com>
2024-11-09 15:42:09 -0600

    added new struct for user, registration request/response, and login request/response

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,42 +1,66 @@
 package handlers
 
-
 import (
 	"encoding/json"
+	"log/slog"
+	"math"
 	"net/http"
-	
+	"os"
+	"strings"
+
 	"github.com/mhson281/backend-api/internal/models"
 )
 
 func HandleCalculation(w http.ResponseWriter, r *http.Request) {
 	var req models.CalculationRequest
 
+	// Initialize logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	// Decode the JSON request body
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		http.Error(w, "Invalid request body.  Ensure the operands are valid numbers and requests are in JSON format", http.StatusBadRequest)
+		return
+	}
+
+	// Declare valid operations
+	validOperations := map[string]bool{
+		"add":      true,
+		"subtract": true,
+		"multiply": true,
+		"divide":   true,
+	}
+
+	if !validOperations[strings.ToLower(req.Operation)] {
+		logger.Warn("Unsupported operation attempted", "operation", req.Operation)
+		http.Error(w, "Unsupported operation attempted", http.StatusBadRequest)
+	}
+
+	if math.IsNaN(req.Operand1) || math.IsNaN(req.Operand2) {
+		http.Error(w, "Operands must be valid numbers", http.StatusBadRequest)
 		return
 	}
 
 	// Perform calculation based on request
 	var res models.CalculationResponse
+	res.Operand1 = req.Operand1
+	res.Operand2 = req.Operand2
+	res.Operation = req.Operation
 
-	switch req.Operation {
+	switch strings.ToLower(req.Operation) {
 	case "add":
-	  res.Result = req.Operand1 + req.Operand2
+		res.Result = req.Operand1 + req.Operand2
 	case "subtract":
-	  res.Result = req.Operand1 - req.Operand2
+		res.Result = req.Operand1 - req.Operand2
 	case "multiply":
-	  res.Result = req.Operand1 * req.Operand2
+		res.Result = req.Operand1 * req.Operand2
 	case "divide":
-	  if req.Operand2 == 0 {
+		if req.Operand2 == 0 {
 			res.Error = "Unable to divide by zero"
 			w.WriteHeader(http.StatusBadRequest)
 		} else {
 			res.Result = req.Operand1 / req.Operand2
 		}
-	default:
-	  http.Error(w, "Unsupported operation", http.StatusBadRequest)
-	  return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,29 @@
 package models
 
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RegisterResponse struct {
+	Message string `json:"message"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
 type CalculationRequest struct {
 	Operation string  `json:"operation"`
 	Operand1  float64 `json:"operand1"`
